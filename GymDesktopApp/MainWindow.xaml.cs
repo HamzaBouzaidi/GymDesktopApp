4373e227f73f62a2969a98f10fc161cfdf821ea6
HamzaBouzaidi <Hamzabzd91@gmail.com>
2025-08-17 23:23:17 +0300

    Initial commit for Gym Desktop App

--- GymDesktopApp/MainWindow.xaml.cs
+++ GymDesktopApp/MainWindow.xaml.cs
@@ -16,9 +16,18 @@ namespace GymDesktopApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+      
         public MainWindow()
         {
             InitializeComponent();
+            
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
         }
     }
 }
