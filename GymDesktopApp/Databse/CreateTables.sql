c14d40485e8691cee6824269a222da6c720ea792
HamzaBouzaidi <Hamzabzd91@gmail.com>
2025-08-26 23:48:48 +0300

    linked the app to a database and created first query

---
+++ GymDesktopApp/Databse/CreateTables.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE users (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    username VARCHAR(MAX) NULL,
+    password VARCHAR(MAX) NULL,
+    role VARCHAR(MAX) NULL,
+    status VARCHAR(MAX) NULL,
+    created_at DATE NULL
+);
+
+
+SELECT * FROM users;
